adad04c72adf3fb30f06013fe5c8716a296bca6b
YpzarJohn24 <ypzarjohn24@gmail.com>
2024-01-01 16:40:27 +0800

    Adding components pages and redux

--- frontend/src/pages/Auth/Navigation.jsx
+++ frontend/src/pages/Auth/Navigation.jsx
@@ -11,12 +11,16 @@ import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import "./Navigation.css";
+import { useSelector, useDispatch } from "react-redux";
+import { useLoginMutation } from "../../redux/api/usersApiSlice";
+import { logout } from "../../redux/features/auth/authSlice";
 
 const Navigation = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   const [dropdownOpen, setDropdownOpen] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
 
-  const toggleDropdow = () => {
+  const toggleDropdown = () => {
     setDropdownOpen(!dropdownOpen);
   };
 
@@ -28,6 +32,20 @@ const Navigation = () => {
     setShowSidebar(false);
   };
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [logoutApiCall] = useLoginMutation();
+
+  const logoutHandler = async () => {
+    try {
+      await logoutApiCall().unwrap();
+      dispatch(logout());
+      navigate("/login");
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div
       style={{ zIndex: 999 }}
@@ -65,6 +83,19 @@ const Navigation = () => {
           <FaHeart className="mr-2 mt-[3rem]" style={26} />
           <span className="hidden nav-item-name mt-[3rem]">FAVORITES</span>
         </Link>
+      </div>
+
+      <div className="relative">
+        <button
+          onClick={toggleDropdown}
+          className="flex items-center text-gray-8000 focus:outline-none"
+        >
+          {userInfo ? (
+            <span className="text-white">{userInfo.username}</span>
+          ) : (
+            <></>
+          )}
+        </button>
       </div>
 
       <ul>
