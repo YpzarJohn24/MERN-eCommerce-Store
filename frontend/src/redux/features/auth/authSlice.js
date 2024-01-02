27953df242b6fd4cdef93faee0ab2102a87614b1
YpzarJohn24 <ypzarjohn24@gmail.com>
2024-01-02 13:40:18 +0800

    User Management

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -12,7 +12,7 @@ const authSlice = createSlice({
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
-      const expirationTime = new Date().getItem() + 30 * 24 * 60 * 60 * 1000;
+      const expirationTime = new Date().getTime() + 30 * 24 * 60 * 60 * 1000;
       localStorage.setItem("expirationTime", expirationTime);
     },
 
